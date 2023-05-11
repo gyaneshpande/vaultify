27438c092cfa9179239b2c1e7667a790399d55f6
sathya Bhat <sathya.bhat@idfy.com>
2023-05-11 17:00:31 +0530

    added models and requirements

---
+++ models/models.py
@@ -0,0 +1,35 @@
+from mongoengine import *
+
+conn = connect('asd',host='localhost', port=27017, username='username', password='super-secret-password')
+
+'''
+For inserting into db -> a=Document(key=value) 
+                         a.save()
+For Querying  -> Document.objects(key=value)
+                 Document.objects(price__gt=10000) ==> https://www.tutorialspoint.com/mongoengine/mongoengine_query_operators.htm
+for multiple filters add-> .filter(key=value)
+For Update -> a=Document.objects(key=value).update_one(set__key=value)
+              a.save()
+For Delete -> Document.objects(key=value).delete()
+'''
+
+class UserEntity(Document):
+    Name = StringField(max_length=50)
+    TimeStamp = ComplexDateTimeField()
+    ApiKey = UUIDField()
+    Status = StringField(max_length=10)
+    meta = {'allow_inheritance': True}
+
+class ObjectEntity(Document):
+    Uid = ReferenceField('UserEntity', reverse_delete_rule=CASCADE)
+    Data = DictField()
+    TimeStamp = ComplexDateTimeField()
+
+class Config(Document):
+    Uid = ReferenceField('UserEntity', reverse_delete_rule=CASCADE)
+    Rules = DictField()
+
+class Billing(Document):
+    Uid = ReferenceField('UserEntity', reverse_delete_rule=DO_NOTHING)
+    CallType = StringField(max_length=10)
+    Oid = ReferenceField('UserEntity', reverse_delete_rule=DO_NOTHING)
